d0ba36d325ca4685946119285e39b93897326d09
wjjhhh <wujiajie@corp.yingzi.com>
2024-08-13 18:42:22 +0800

    feat: 增加唤醒

--- src/components/Asr/index.vue
+++ src/components/Asr/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import Iat from "./iat";
+import pinyin from 'pinyin';
 
 type Status = "UNDEFINED" | "OPEN" | "CLOSING" | "CLOSED";
 
@@ -28,9 +29,61 @@ Iat.onTextChange = (text: string) => {
   asrText.value = text;
 };
 
+const isMatch = (texts = '') => {
+  if (texts.length < 2) {
+    return false
+  }
+  const last2Texts = pinyin(texts.slice(-2))
+  return last2Texts[0][0].startsWith('xi') && (last2Texts[1][0].startsWith('yǐn') || 
+  last2Texts[1][0].startsWith('yīn')
+  || last2Texts[1][0].startsWith('yín')
+  )
+}
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.continuous = true; // 默认false，用户停止说话时，语音识别将结束
+recognition.interimResults = true; // 默认值false，代表语音识别器的返回值不会改变
+recognition.onstart = function () {
+  // console.log("onstart");
+};
+recognition.onresult = (event) => {
+  console.log("onresult", event);
+  let interim_transcript = "";
+  for (var i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+    } else {
+      interim_transcript += event.results[i][0].transcript;
+    }
+  }
+  if (!interim_transcript ) {
+    return
+  }
+  if (isMatch(interim_transcript)) {
+    if (status.value === 'OPEN') {
+      stop()
+    }
+    start()
+  }
+};
+recognition.onend = function () {
+  // console.log("onend");
+  wake()
+};
+recognition.onerror = function (event) {
+  console.error("onerror", event);
+};
+
+// 开始唤醒
+const wake = () => {
+  recognition.lang = "zh-CN";
+  recognition.start();
+};
 </script>
 
 <template>
+  <button @click="wake" class="wake-button">开始持续唤醒</button>
   <button @click="start" :disabled="!['CLOSED', 'UNDEFINED'].includes(status)">开始拾音</button>
   <button @click="stop" :disabled="status !== 'OPEN'">结束拾音</button>
   状态: {{ status }}
@@ -41,5 +94,12 @@ Iat.onTextChange = (text: string) => {
 <style scoped>
 textarea {
   width: 400px;
+}
+.wake-button {
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: #000;
+  color: #fff;
 }
 </style>
